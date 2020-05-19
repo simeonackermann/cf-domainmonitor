9f55d7ce258fa6c41c94ac0c268ef079403aa918
Simeon Ackermann <amseon@web.de>
2020-05-19 12:43:00 +0200

    Update packages json

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -4,5 +4,5 @@ if [ "$TRAVIS_OS_NAME" == osx ]; then
     yarn release
 else
     # deploy on linux
-    docker run --rm -e GH_TOKEN=$GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux"
+    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux"
 fi
