7184d1a808ec3ac721415ae39e2112eeaab4b308
Simeon Ackermann <amseon@web.de>
2020-05-19 10:49:25 +0200

    Update: add release command

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 if [ "$TRAVIS_OS_NAME" == osx ]; then
     # deploy on mac
-    yarn make-mac
+    yarn release
 else
     # deploy on linux
-    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn make-linux"
+    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux"
 fi
