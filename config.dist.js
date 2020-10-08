eb454afa11383caa31c4c738632c7e89ba679727
Simeon Ackermann <amseon@web.de>
2020-10-08 11:20:45 +0200

    Update config.dist

--- config.dist.js
+++ config.dist.js
@@ -3,12 +3,11 @@ var config = {
     development: {
         apimail: "",
         apikey: "",
-        limit: 10
+        limit: 10 // only load x entries to make the request faster
     },
     production: {
         apimail: "",
         apikey: "",
-        limit: 10
     }
 };
 
