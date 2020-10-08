d670e9f7c27f489bda57de01141ce1bf853e9d51
Simeon Ackermann <amseon@web.de>
2020-10-08 11:00:10 +0200

    Add example config

---
+++ config.dist.js
@@ -0,0 +1,15 @@
+
+var config = {
+    development: {
+        apimail: "",
+        apikey: "",
+        limit: 10
+    },
+    production: {
+        apimail: "",
+        apikey: "",
+        limit: 10
+    }
+};
+
+module.exports = config;
